859974fc8d8c567e492d3883a0fed85ea5899987
Juniven Saavedra <junivensaavedra@gmail.com>
2026-04-16 23:38:33 +0800

    Add sales fact view to gold layer initialization

--- scripts/init_gold_views.sql
+++ scripts/init_gold_views.sql
@@ -72,3 +72,24 @@ SELECT
 FROM silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc ON pn.cat_id = pc.id
 WHERE pn.prd_end_dt IS NULL;
+
+
+-- ============================================
+-- Fact: Sales
+-- ============================================
+
+CREATE OR REPLACE VIEW gold.fact_sales AS
+SELECT
+    sd.sls_ord_num  as order_number,
+    pr.product_key,
+    cu.customer_key,
+    sd.sls_order_dt as order_date,
+    sd.sls_ship_dt  as ship_date,
+    sd.sls_due_dt   as due_date,
+    sd.sls_sales    as sales_amount,
+    sd.sls_quantity as quantity,
+    sd.sls_price    as price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr  ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu ON sd.sls_cust_id = cu.customer_id;
+
