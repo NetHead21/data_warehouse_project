4fd7389a0b6776f539523d5234a2bf2157ab05fa
Juniven Saavedra <junivensaavedra@gmail.com>
2026-04-16 23:37:55 +0800

    Add customer dimension view to gold layer initialization

--- scripts/init_gold_views.sql
+++ scripts/init_gold_views.sql
@@ -27,3 +27,26 @@ Warning:
       definitions. No data will be lost.
 ===============================================
 */
+
+
+-- ============================================
+-- Dimension: Customers
+-- ============================================
+
+CREATE OR REPLACE VIEW gold.dim_customers AS
+SELECT row_number() over (order by ci.cst_id) as customer_key,
+       ci.cst_id                              as customer_id,
+       ci.cst_key                             as customer_number,
+       ci.cst_firstname                       as first_name,
+       ci.cst_lastname                        as last_name,
+       ci.cst_marital_status                  as marital_status,
+       la.cntry                               as country,
+       case
+           when ci.cst_gndr != 'n/a' then ci.cst_gndr
+           else coalesce(ca.gen, 'n/a')
+           end                                as gender,
+       ca.bdate                               as birthdate,
+       ci.cst_create_date                     as create_date
+FROM silver.crm_cust_info ci
+         LEFT JOIN silver.erp_cust_az12 ca ON ci.cst_key = ca.cid
+         LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key = la.cid;
