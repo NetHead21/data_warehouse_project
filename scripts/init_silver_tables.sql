284b17c31f241f5813268588015aa219ead1f22f
Juniven Saavedra <junivensaavedra@gmail.com>
2026-04-16 23:36:48 +0800

    Add ERP location and category tables to silver layer initialization

--- scripts/init_silver_tables.sql
+++ scripts/init_silver_tables.sql
@@ -85,3 +85,21 @@ create table silver.erp_cust_az12 (
     gen             varchar(10),
     dwh_create_date date default current_date
 );
+
+
+drop table if exists silver.erp_loc_a101;
+create table silver.erp_loc_a101 (
+    cid             varchar(50),
+    cntry           varchar(50),
+    dwh_create_date date default current_date
+);
+
+
+drop table if exists silver.erp_px_cat_g1v2;
+create table silver.erp_px_cat_g1v2 (
+    id              varchar(50),
+    cat             varchar(50),
+    subcat          varchar(50),
+    maintenance     varchar(10),
+    dwh_create_date date default current_date
+);
